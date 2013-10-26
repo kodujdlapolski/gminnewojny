96b020b9061a824346b1d913be9bbf63bfe1c859
m <wrochniak@gmail.com>
2013-10-26 05:22:15 +0200

    Finding a similiar city is more fuzzy

--- cities.py
+++ cities.py
@@ -2,6 +2,19 @@ import csv
 import re
 import bisect
 
+def _depolonize(text):
+    polish_letters_map = {"\xc4\x84": "A", "\xc4\x86": "C",
+                          "\xc4\x98": "E", "\xc5\x81": "L", "\xc5\x83": "N", "\xc3\x93": "O",
+                          "\xc5\x9a": "S", "\xc5\xb9": "Z", "\xc5\xbb": "Z", "\xc4\x85": "a",
+                          "\xc4\x87": "c", "\xc4\x99": "e", "\xc5\x82": "l", "\xc5\x84": "n",
+                          "\xc3\xB3": "o", "\xc5\x9b": "s", "\xc5\xba": "z", "\xc5\xbc": "z"}
+    for from_what, to_what in polish_letters_map.iteritems():
+        text = text.replace(from_what, to_what)
+    return text
+
+def _normalize(text):
+    return _depolonize(text).lower()
+
 class Cities:
 
     def __init__(self):
@@ -17,7 +30,7 @@ class Cities:
                 city = row[0]
                 population = int(no_digits_re.sub("", row[4]))
 
-                self._city_to_population[city] = population
+                self._city_to_population[_normalize(city)] = population
                 self._populations.append(population)
                 if population in self._population_to_city:
                     self._population_to_city[population].append(city)
@@ -27,7 +40,7 @@ class Cities:
             self._populations.sort()
 
     def get_population_for_city(self, city):
-        return self._city_to_population[city]
+        return self._city_to_population[_normalize(city)]
     
     def get_some_city_for_population(self, population):
         return self._population_to_city[population][0]
